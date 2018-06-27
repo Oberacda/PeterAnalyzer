1946d795531b9198ac09016650d402d675ffe78d
David Oberacker <david.oberacker@student.kit.edu>
2018-06-27 15:16:23 +0200

    Added csv exporter

---
+++ peter_analyzer/csv_export.py
@@ -0,0 +1,21 @@
+import csv
+
+if __name__ == '__main__':
+    with open('eggs.csv', 'w', newline='') as csvfile:
+        csv.excel_tab
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel',quoting=csv.QUOTE_NONNUMERIC,delimiter=';')
+
+        writer.writeheader()
+        writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+        writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+
+

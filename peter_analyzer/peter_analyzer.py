654980b10ffde304bf4093533939e8f696cde0e2
David Oberacker <david.oberacker@student.kit.edu>
2018-07-01 17:27:50 +0200

    Refined command line options.

--- peter_analyzer/peter_analyzer.py
+++ peter_analyzer/peter_analyzer.py
@@ -11,13 +11,25 @@ if __name__ == "__main__":
                         dest="logdir",
                         help="destination of the logfiles",
                         default="./logs/",
-                        metavar="FOLDER")
+                        metavar="DIR")
 
     parser.add_argument("-q", "--quiet",
                         action="store_false",
                         dest="verbose",
                         default=True,
                         help="don't print log info messages to stdout")
+
+    parser.add_argument("-i", "--input",
+                        dest="input",
+                        help="Json input file containing the data.",
+                        required=True,
+                        metavar="FILE.json")
+
+    parser.add_argument("-o", "--output-dir",
+                        dest="output",
+                        default=".",
+                        help="Output dir for the data.",
+                        metavar="DIR")
 
     args = parser.parse_args()
 
@@ -28,7 +40,6 @@ if __name__ == "__main__":
     # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
-
 
     if args.verbose :
 
@@ -56,10 +67,7 @@ if __name__ == "__main__":
         else:
             log.error('Specified log dir is no directory')
     else:
-
-        home = Path('.')
-        home = home.joinpath(p)
-        logfile = home.joinpath(logfile)
+        logfile = Path.cwd().joinpath(p).joinpath(logfile)
         logfile.parent.mkdir(parents=True, exist_ok=True)
 
         fh = logging.FileHandler(logfile)
@@ -71,11 +79,37 @@ if __name__ == "__main__":
 
         log.addHandler(fh)
 
+    output_path = Path(args.output)
+    if output_path.exists():
+        if output_path.is_dir():
+            log.info("Output path :" + str(output_path.resolve()))
+
+        else:
+            log.error("Output param not a directory using default.")
+            output_path = Path.cwd()
+
+    else:
+        output_path = Path.cwd().joinpath(output_path)
+        output_path.mkdir(parents=True, exist_ok=True)
+        log.info("Output path :" + str(output_path.resolve()))
+
+    input_path = Path(args.input)
+    if input_path.exists():
+        if input_path.is_file():
+            log.info("Input path :" + str(input_path.resolve()))
+            if not input_path.suffix == ".json":
+                log.error("Input file not a json file.")
+                exit(-1)
 
 
 
+        else:
+            log.error("Input param not a file.")
+            exit(-1)
 
-
+    else:
+        log.error("Input param file or folder does not exist!")
+        exit(-1)
 
 
 

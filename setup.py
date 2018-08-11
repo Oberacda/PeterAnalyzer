029e808c1e1bdb42d87ef02b3331bc53b744e7cc
David Oberacker <david.oberacker@student.kit.edu>
2018-08-11 22:09:06 +0200

    Added release steps and setup.py.

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup
 setup(
     name='PeterAnalyzer',
     version='1.0.0',
-    packages=['peter_analyzer'],
+    packages=['peter_analyzer', 'peter_analyzer.data_set_creator'],
     url='',
     license='GPL-3.0',
     author='David Oberacker',
     author_email='david.oberacker@gmail.com',
-    description='Analysis tool for Peter output files.'
+    description='Analysis tool for Peter output files.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "matplotlib >= 2.2.0",
+        "DateTime >= 4.2",
+        "coloredlogs"
+    ],
+    scripts=['src/peter_csv_analyzer.py'],
+    package_dir={'peter_analyzer': 'src/peter_analyzer'},
 )
